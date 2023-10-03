f5f069290e5069d59ce8b5030b61bd4b46199532
Deer0806 <geng.lu@northeastern.edu>
2023-10-02 20:41:38 -0700

    Create an app for Custom table view With XIB file to show different food items

---
+++ CustomTableViewWithXIB/CustomTableViewWithXIB/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  CustomTableViewWithXIB
+//
+//  Created by 耿露 on 10/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let imageNames = ["BBQ", "ChineseBBQ", "Duck", "Taco", "Korean", "Curry", "DimSum", "Hotpot",]
+    
+    @IBOutlet weak var tblView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = Bundle.main.loadNibNamed("ImageTableViewCell", owner: self)?.first as! ImageTableViewCell
+        cell.imgView?.image = UIImage(named: imageNames[indexPath.row])
+        cell.lblImage.text = imageNames[indexPath.row]
+        
+        
+        return cell
+    }
+    
+
+}
+
