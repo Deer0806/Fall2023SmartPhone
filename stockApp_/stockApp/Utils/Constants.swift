5dd9aebfce4220a27f2ba82aacb1b33a3662854a
Deer0806 <geng.lu@northeastern.edu>
2023-10-21 17:09:55 -0700

    Add stock

---
+++ stockApp_/stockApp/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  stockApp
+//
+//  Created by 耿露 on 10/21/23.
+//
+
+import Foundation
+
+let apiKey = "267ccd7ac4c33822e9a5b82705254f63"
+let baseURL = "https://financialmodelingprep.com/api/v3/"
